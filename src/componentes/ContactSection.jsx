88e2de133d6c2cac01297e614b32bacccdda88ec
Daniel121826 <alumno709866@ies-azarquiel.es>
2026-02-03 12:27:20 +0100

    responsive

--- src/componentes/ContactSection.jsx
+++ src/componentes/ContactSection.jsx
@@ -9,12 +9,12 @@ export default function ContactSection() {
     >
       <div className="absolute inset-0 bg-black/50"></div>
 
-      <div className="relative w-[90%] mx-auto flex flex-col md:flex-row items-center md:items-center gap-12 p-8 rounded-lg">
+      <div className="relative w-[90%] mx-auto flex flex-col md:flex-row md:items-center gap-12 p-4">
         
         <div className="flex-1 text-white">
           <h2 className="text-3xl md:text-4xl font-bold mb-4">Bienvenido Artista</h2>
           <p className="text-base md:text-lg leading-relaxed">
-            Aidan Creative es la empresa numero 1 en servicios creativos para empresas. Nos encargaremos de dar color, vida y creatividad a tu negocio a través de nuestros servicios de diseño gráfico, marketing digital y desarrollo web.
+            Aidan Creative es la empresa numero uno en servicios creativos para empresas. Nos encargaremos de dar color, vida y creatividad a tu negocio a través de nuestros servicios de diseño gráfico, marketing digital y desarrollo web.
             <br /><br />
             Contáctanos y descubre cómo podemos colaborar para que tu empresa alcance el siguiente nivel.
           </p>
